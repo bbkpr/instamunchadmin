ffe2135a9237133621b9acb948d84a2aa0580964
Robert Parker <whatman@gmail.com>
2024-12-24 09:59:40 -0800

    fix env vars in vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,19 +1,19 @@
-import path from 'node:path'
+import path from 'node:path';
 
-import EnvironmentPlugin from 'vite-plugin-environment'
-import { defineConfig } from 'vitest/config'
+import EnvironmentPlugin from 'vite-plugin-environment';
+import { defineConfig } from 'vitest/config';
 export default defineConfig({
-  plugins: [EnvironmentPlugin(['REACT_APP_TEXT']) as any],
+  plugins: [EnvironmentPlugin(['VITE_API_BASE_URL', 'VITE_GRAPHQL_URI']) as any],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+      '@': path.resolve(__dirname, './src')
+    }
   },
   test: {
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./src/setupTests.ts'],
     include: ['src/**/*.{test,spec}.{ts,tsx}'],
-    exclude: ['src/@types', 'node_modules'],
-  },
-})
+    exclude: ['src/@types', 'node_modules']
+  }
+});
